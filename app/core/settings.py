b714e5b8614f43e62f0320d0fb17d0ced2df9e6d
Andrei Burvel <burvelandrei@gmail.com>
2026-05-24 14:08:49 +0300

    chore: added email settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -149,5 +149,14 @@ CELERY_BROKER_TRANSPORT_OPTIONS = {
 CELERY_TASK_SERIALIZER = "json"
 CELERY_ENABLE_UTC = True
 
+# Email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+
 if not logging.getLogger().handlers:
     logging.config.dictConfig(get_logging_settings(debug=DEBUG))
