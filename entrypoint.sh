d1c1e39589549423152c5ae16b77a3f080f3087f
Andrei Burvel <burvelandrei@gmail.com>
2026-05-24 18:21:32 +0300

    fix: fix bugs with staticfiles

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 echo "Running migrations..."
 python manage.py migrate
 
+echo "Collecting static files..."
+python manage.py collectstatic --no-input --clear
+
 echo "Starting application..."
 exec "$@"
