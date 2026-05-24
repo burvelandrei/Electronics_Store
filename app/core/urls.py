8a03cc1b51d991a4b9139a4d23e4cd4523c97f83
Andrei Burvel <burvelandrei@gmail.com>
2026-05-24 16:09:51 +0300

    feat: added api auth for login

--- app/core/urls.py
+++ app/core/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/stores/", include("stores.urls")),
     path("api/products/", include("products.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
